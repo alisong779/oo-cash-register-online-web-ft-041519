8fa5c91a732bc77b68b9c41ea0326208d71986dc
Alison Golden <alisong779@yahoo.com>
2019-04-24 15:25:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,7 @@ class CashRegister
     if discount > 0 
       disc_total = (discount * price)/100
       @total -= disc_total
-        puts "After the discount, the total comes to #{@total}."
+        puts "After the discount, the total comes to #{disc_total}."
   end 
 end 
 end 
