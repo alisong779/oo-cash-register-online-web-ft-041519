2cbb2304186d064edd9eef331b931c5301fe5602
Alison Golden <alisong779@yahoo.com>
2019-04-24 15:36:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -28,6 +28,10 @@ class CashRegister
          "After the discount, the total comes to $#{@total}."
     else 
         "There is no discount to apply."
-  end 
-end 
+    end 
+  end
+  
+    def void_last_transaction
+      
+    end 
 end 
